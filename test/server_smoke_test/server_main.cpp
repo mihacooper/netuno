c3fcad6164b799c08a59f587826e95f0d78d0782
mihacooper <mihakuper@gmail.com>
2017-02-21 12:31:43 +0300

    adapt server test for new generation policy

--- test/server_smoke_test/server_main.cpp
+++ test/server_smoke_test/server_main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "SampleInterface.h"
+#include "sample.hpp"
 
 int main()
 {
--- test/server_smoke_test/test_smoke.sh
+++ test/server_smoke_test/test_smoke.sh
@@ -6,14 +6,12 @@ cd $WORK_DIR
 cp $TEST_DIR/sample.lua .
 
 $SDK_DIR/main.lua $TEST_DIR/sample.lua SampleInterface cpp server
-testf_assert [ -f "SampleInterface.cpp" ]
-testf_assert [ -f "SampleInterface.h"   ]
-testf_assert [ -f "SampleStructure.cpp" ]
-testf_assert [ -f "SampleStructure.h"   ]
+testf_assert [ -f "sample.cpp" ]
+testf_assert [ -f "sample.hpp"   ]
 
 testf_assert g++ -std=c++14 \
-    $TEST_DIR/server_main.cpp SampleInterface.cpp SampleStructure.cpp \
-    -I$PWD -I../../LuaBridge -I/usr/include/lua5.2 -I$SDK_DIR \
+    $TEST_DIR/server_main.cpp sample.cpp \
+    -I/usr/include/lua5.2 -I$SDK_DIR -I$WORK_DIR \
     -llua5.2 -o sample
 
 testf_assert ./sample
